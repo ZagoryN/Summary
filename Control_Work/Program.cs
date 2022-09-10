41fa910409a1dbbce1f7817075ef5ea234dfa86c
Nina <nzagorevskaia@gmail.com>
2022-09-10 13:18:51 +0300

    Added block diagram and first method

--- Control_Work/Program.cs
+++ Control_Work/Program.cs
@@ -7,3 +7,26 @@
 [“Hello”, “2”, “world”, “:-)”] → [“2”, “:-)”]
 [“1234”, “1567”, “-2”, “computer science”] → [“-2”]
 [“Russia”, “Denmark”, “Kazan”] → [] */
+
+void FillArray(string?[] array)  //метод наполнения массива
+{
+    int length = array.Length;
+    int index = 0;
+    Console.WriteLine("Введите через Enter элементы массива:");
+    while (index < length)
+    {
+        array[index] = Console.ReadLine();
+        index++; 
+    }
+    
+}
+
+Console.Write("Введите количество элементов массива: ");
+int numberOfElements = Convert.ToInt32(Console.ReadLine());
+
+string[] arrayOfString;
+arrayOfString = new string[numberOfElements];
+
+FillArray(arrayOfString);
+Console.WriteLine($"Исходный массив: [{string.Join(", ", arrayOfString)}]"); // напечатаем получившийся массив
+
